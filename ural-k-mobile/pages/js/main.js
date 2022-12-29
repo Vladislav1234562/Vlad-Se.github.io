1fa255de2c35108bf14b5c8d9f2aa65e34da41d4
Vlad-Se <100925058+Vlad-Se@users.noreply.github.com>
2022-12-29 23:50:34 +0500

    step01

--- ural-k-mobile/pages/js/main.js
+++ ural-k-mobile/pages/js/main.js
@@ -214,7 +214,7 @@ $(document).ready(function(e) {
 		},200);
 	});
 	/* Млдалка корзины */
-	$('.tovars__item-cart, .novelty__item-cart').click(function(e){
+	$('.tovars__item-cart, .novelty__item-cart, .cart-b__item-cart').click(function(e){
 		lockScroll();
 		e.preventDefault();
 		$('.page-shadow').fadeIn(200);
